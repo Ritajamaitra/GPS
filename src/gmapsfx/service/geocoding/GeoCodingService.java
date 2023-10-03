16509bb9422bb72fcba7a5952ef93452179c179d
Ritajamaitra <75163189+Ritajamaitra@users.noreply.github.com>
2023-10-03 22:03:40 +0530

    initial commit

---
+++ src/gmapsfx/service/geocoding/GeoCodingService.java
@@ -0,0 +1,14 @@
+package gmapsfx.service.geocoding;
+
+import gmapsfx.javascript.JavascriptObject;
+
+/**
+ *
+ 
+ */
+public class GeoCodingService extends JavascriptObject {
+    
+    
+    
+    
+}
